bbca6d7ae106c0f49a3252074735a4990751845a
Adham Ghazali <adham.ghazali@gmail.com>
2026-01-20 17:59:16 +0200

    Switch to standard remoroo-core dependency and bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="remoroo",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
         "typer>=0.9.0",
         "jsonschema>=4.22.0",
         "requests>=2.31.0",
-        "remoroo-core @ git+https://github.com/Remoroo/remoroo_core.git@main",
+        "remoroo-core>=0.1.0",
         "sseclient-py",
     ],
     entry_points={
